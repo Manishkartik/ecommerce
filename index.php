516c80daf282b0b7eed8771c09900988ecd97d02
Manish kumar <manishkartiks@gmail.com>
2018-05-27 18:38:14 +0530

    first commit

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php $ptitle="e-Mart | Home";?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/includes/head.php';?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/includes/menu.php';?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/content_php_file/home.php';?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/includes/footer.php';?>
--- sarees.php
+++
@@ -1,5 +0,0 @@
-<?php $ptitle="e-Mart | Sarees";?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/includes/head.php';?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/includes/menu.php';?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/content_php_file/sarees_content.php';?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/ecommerce/includes/footer.php';?>
